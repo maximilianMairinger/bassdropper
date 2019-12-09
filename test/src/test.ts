f69a8b6c3abc4ec94b0341641bcbf9ec0c2666a0
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2019-12-09 21:54:26 +0100

    work

--- test/src/test.ts
+++ test/src/test.ts
@@ -1,4 +1,26 @@
 import bassdropper from "../../app/src/bassdropper"
-const testElem = document.querySelector("#test")
+const elem: HTMLAudioElement = document.querySelector("audio")
 
 
+// document.querySelector("button").addEventListener("click", () => {
+//   elem.currentTime = 10
+//   elem.play()
+// })
+
+
+let input = document.querySelectorAll("input")
+let timeOut: any;
+
+document.body.addEventListener("keydown", (e) => {
+  if (e.target === document.body) {
+    elem.currentTime = +input[0].value || 40
+    
+    elem.play()
+
+    if (timeOut !== undefined) clearTimeout(timeOut)
+    timeOut = setTimeout(() => {
+      elem.pause()
+    }, (+input[1].value || 20) * 1000)
+  }
+  
+})
